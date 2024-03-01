9107562794272d914cf993dc2a1378d23dca5291
Erick Chaparro <erickenriquechaparro@gmail.com>
2024-02-29 20:39:49 -0500

    Initial commit

---
+++ app/src/main/java/primera/mi_app/Mainclass.java
@@ -0,0 +1,15 @@
+package primera.mi_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mainclass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_class);
+    }
+
+}
